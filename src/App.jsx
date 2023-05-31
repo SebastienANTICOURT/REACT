2136c47b0e34bbebd092b12b02bb6472b9c728fc
SebastienANTICOURT <sebastien.anticourt@live.fr>
2023-05-31 09:33:41 +0200

    quete 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import './App.css'
 import PokemonCard from "./components/PokemonCard"
 
 function App() {
-  const [count, setCount] = useState(0)
+  /*const [count, setCount] = useState(0)*/
 
   return (
     <>
-      <div>PokemonCard /</div>
+      <div> <PokemonCard /></div>
     </>
   );
 }
